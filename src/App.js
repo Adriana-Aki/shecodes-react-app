640b389bdd90073adb9d3141d3bea463525446d3
Adriana-Aki <adriana.aki157@gmail.com>
2024-09-27 14:18:43 +0200

    header

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Weather app</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather app</h1>
       </header>
     </div>
   );
